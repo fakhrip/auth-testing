85ee184909bde28101f0fec283c64fe5d91e76ed
fakhrip <fakhrip@protonmail.com>
2024-04-16 22:42:38 +0700

    add and initialize mikro-orm library

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      entities: ['dist/**/*.entity.js'],
+      entitiesTs: ['src/**/*.entity.ts'],
+      metadataProvider: TsMorphMetadataProvider,
+      driver: PostgreSqlDriver,
+
+      host: 'localhost',
+      port: 3307,
+      user: 'root',
+      password: '',
+      dbName: 'dbname',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
