43ef8c9de781f9b68502a95aedeabffcb1257e06
fakhrip <fakhrip@protonmail.com>
2024-04-17 07:26:08 +0700

    integrate logout feature button to frontend

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -5,21 +5,34 @@ import { AuthContextType } from "../types/user"
 export function HomeLayout() {
   const { user } = useAuth() as AuthContextType
 
-  let fetcher = useFetcher();
-  let isDeletingAccount = fetcher.formData != null;
+  let deleteFetcher = useFetcher();
+  let logoutFetcher = useFetcher();
+  let isDeletingAccount = deleteFetcher.formData != null;
+  let isLoggingOut = logoutFetcher.formData != null;
+
   let actionData = useActionData() as { error: string } | undefined;
 
   return (
     <div>
       Hello {user.username}, welcome to your homepage!
-      <fetcher.Form method="post" action="/delete">
+
+      <deleteFetcher.Form method="post" action="/delete">
         <button type="submit" disabled={isDeletingAccount}>
           {isDeletingAccount ? "Deleting account..." : "Delete account"}
         </button>
         {actionData && actionData.error ? (
           <p style={{ color: "red" }}>{actionData.error}</p>
         ) : null}
-      </fetcher.Form>
+      </deleteFetcher.Form>
+
+      <logoutFetcher.Form method="post" action="/logout">
+        <button type="submit" disabled={isLoggingOut}>
+          {isLoggingOut ? "Logging out..." : "Logout"}
+        </button>
+        {actionData && actionData.error ? (
+          <p style={{ color: "red" }}>{actionData.error}</p>
+        ) : null}
+      </logoutFetcher.Form>
     </div>
   )
 }
