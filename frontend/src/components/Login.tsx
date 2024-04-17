b96f511264a28db5c882de3e0b3b5c54bda928d3
fakhrip <fakhrip@protonmail.com>
2024-04-17 07:05:17 +0700

    integrate login feature to frontend

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,11 +1,34 @@
-import { redirect } from "react-router-dom"
+import { LoaderFunctionArgs, redirect, useActionData, useFetcher, useNavigation } from "react-router-dom";
 import { useAuth } from "../AuthContext"
 import { AuthContextType } from "../types/user"
+import { loginAccount } from "../util/auth";
+import { loginFirebase } from "../util/firebase";
 
 export function LoginLayout() {
+  let fetcher = useFetcher();
+
+  let navigation = useNavigation();
+  let isSigningUp = navigation.formData?.get("username") != null || navigation.formData?.get("password") != null;
+
+  let actionData = useActionData() as { error: string } | undefined;
+
   return (
     <div>
       Login
+      <fetcher.Form method="post" action="/users/login">
+        <label>
+          Username: <input name="username" />
+        </label>
+        <label>
+          Password: <input name="password" />
+        </label>
+        <button type="submit" disabled={isSigningUp}>
+          {isSigningUp ? "Logging in..." : "Login"}
+        </button>
+        {actionData && actionData.error ? (
+          <p style={{ color: "red" }}>{actionData.error}</p>
+        ) : null}
+      </fetcher.Form>
     </div>
   )
 }
@@ -20,7 +43,43 @@ export function LoginLoader() {
   return null
 }
 
-export function LoginAction() {
-  // TODO: log the user in
-  return false
+export async function LoginAction({ request }: LoaderFunctionArgs) {
+  const { updateUser } = useAuth() as AuthContextType
+  let formData = await request.formData();
+
+  let username = formData.get("username") as string | null;
+  let password = formData.get("password") as string | null;
+
+  if (!username) {
+    return {
+      error: "You must provide a username to login",
+    };
+  }
+
+  if (!password) {
+    return {
+      error: "You must provide a password to login",
+    };
+  }
+
+  try {
+    const response = await loginAccount(username, password)
+    if (response.responseCode === 200) {
+      updateUser({
+        username: response.jsonResponse.username,
+        token: response.jsonResponse.token,
+        ...await loginFirebase(response.jsonResponse.token)
+      })
+
+      return redirect("/home");
+    }
+
+    return {
+      error: response.jsonResponse.message.errors ?? response.jsonResponse.message ?? response.jsonResponse,
+    };
+  } catch (error) {
+    return {
+      error,
+    };
+  }
 }
--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -78,5 +78,4 @@ export async function SignupAction({ request }: LoaderFunctionArgs) {
       error,
     };
   }
-
 }
